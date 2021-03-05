d9ebbfc1422d3808e0514943492d198d6b6603db
mot3 <hopdrmot3@gmail.com>
2021-03-05 17:33:17 +0330

    Update bubble sort

--- Sorting Techniques/C++/Bubble_Sort.cpp
+++ Sorting Techniques/C++/Bubble_Sort.cpp
@@ -7,12 +7,12 @@ int main()
   int a[5] = {10, 2, 0, 43, 12};
 
   cout << "Input list ...\n";
-  for (int i = 0; i < 5; i++)
+  for (size_t i = 0; i < 5; i++)
     cout << a[i] << "\t";
 
   cout << endl;
 
-  for (int i = 0; i < 5; i++)
+  for (size_t i = 0; i < 5; i++)
     for (int j = i + 1; j < 5; j++)
     {
       if (a[i] > a[j])
