b119f735ab869fbaaf9d151e7b32c32b1c99dc76
mot3 <hopdrmot3@gmail.com>
2021-03-05 20:41:26 +0330

    change tab to 4

--- Sorting Techniques/C++/Selection_Sort.cpp
+++ Sorting Techniques/C++/Selection_Sort.cpp
@@ -5,44 +5,44 @@ int findSmallest(int[], int);
 
 int main()
 {
-  int myarray[5] = {12, 45, 8, 15, 33};
-  int pos, temp;
+    int myarray[5] = {12, 45, 8, 15, 33};
+    int pos, temp;
 
-  for (size_t i = 0; i < 5; i++)
-    cout << myarray[i] << "\t";
+    for (size_t i = 0; i < 5; i++)
+        cout << myarray[i] << "\t";
 
-  for (size_t i = 0; i < 5; i++)
-  {
-    pos = findSmallest(myarray, i);
-    temp = myarray[i];
-    myarray[i] = myarray[pos];
-    myarray[pos] = temp;
-  }
+    for (size_t i = 0; i < 5; i++)
+    {
+        pos = findSmallest(myarray, i);
+        temp = myarray[i];
+        myarray[i] = myarray[pos];
+        myarray[pos] = temp;
+    }
 
-  cout << "\n Sorted list of elements is\n";
+    cout << "\n Sorted list of elements is\n";
 
-  for (size_t i = 0; i < 5; i++)
-    cout << myarray[i] << "\t";
+    for (size_t i = 0; i < 5; i++)
+        cout << myarray[i] << "\t";
 
-  cout << endl;
+    cout << endl;
 
-  return 0;
+    return 0;
 }
 
 int findSmallest(int myarray[], int i)
 {
-  int ele_small, position, j;
-  ele_small = myarray[i];
-  position = i;
+    int ele_small, position, j;
+    ele_small = myarray[i];
+    position = i;
 
-  for (j = i + 1; j < 5; j++)
-  {
-    if (myarray[j] < ele_small)
+    for (j = i + 1; j < 5; j++)
     {
-      ele_small = myarray[j];
-      position = j;
+        if (myarray[j] < ele_small)
+        {
+            ele_small = myarray[j];
+            position = j;
+        }
     }
-  }
 
-  return position;
+    return position;
 }
