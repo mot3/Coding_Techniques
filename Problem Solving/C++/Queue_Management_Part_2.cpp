6546d28511a07a786126c5b228e7067ffde1aea5
mot3 <hopdrmot3@gmail.com>
2021-03-10 00:10:33 +0330

    Update Queue_Management_Part_2.cpp

--- Problem Solving/C++/Queue_Management_Part_2.cpp
+++ Problem Solving/C++/Queue_Management_Part_2.cpp
@@ -8,71 +8,85 @@
 // which contains the elements of the first queue, followed by the elements of the second queue.
 
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-class Queue { 
-	int size; 
-	int* queue; 
-	
-	public:
-	Queue() { 
+class Queue
+{
+	int size;
+	int *queue;
+
+public:
+	Queue()
+	{
 		size = 0;
 		queue = new int[100];
 	}
-	void add(int data) { 
-		queue[size] = data; 
+	void add(int data)
+	{
+		queue[size] = data;
 		size++;
 	}
-	void remove() { 
-		if (size == 0) { 
-			cout << "Queue is empty"<<endl; 
-			return; 
-		} 
-		else { 
-			for (int i = 0; i < size - 1; i++) { 
-				queue[i] = queue[i + 1]; 
-			} 
-			size--; 
-		} 
-	} 
-	void print() { 
-		if (size == 0) { 
-			cout << "Queue is empty"<<endl; 
-			return; 
-		} 
-		for (int i = 0; i < size; i++) { 
-			cout<<queue[i]<<" <- ";
-		} 
+	void remove()
+	{
+		if (size == 0)
+		{
+			cout << "Queue is empty" << endl;
+			return;
+		}
+		else
+		{
+			for (int i = 0; i < size - 1; i++)
+			{
+				queue[i] = queue[i + 1];
+			}
+			size--;
+		}
+	}
+	void print()
+	{
+		if (size == 0)
+		{
+			cout << "Queue is empty" << endl;
+			return;
+		}
+		for (int i = 0; i < size; i++)
+		{
+			cout << queue[i] << " <- ";
+		}
 		cout << endl;
 	}
 
-	Queue operator+ (Queue & obj) {
-    Queue res;
-    res.size = this->size + obj.size;
-    
-    for (size_t i = 0; i < this->size; i++)
-    {
-      res.queue[i] = this->queue[i];
-    }
+	Queue operator+(Queue &obj)
+	{
+		Queue res;
 
-    for (size_t i = this->size, j = 0; i < res.size ; i++, j++)
-    {
-      res.queue[i] = obj.queue[j];
-    }
+		for (size_t i = 0; i < this->size; i++)
+		{
+			res.add(this->queue[i]);
+		}
+		for (size_t i = 0; i < obj.size; i++)
+		{
+			res.add(obj.queue[i]);
+		}
 
-    return res;
-    
-  }
-	
-}; 
+		return res;
+	}
+};
 
-int main() { 
-	Queue q1; 
-	q1.add(42); q1.add(2); q1.add(8);  q1.add(1);
+int main()
+{
+	Queue q1;
+	q1.add(42);
+	q1.add(2);
+	q1.add(8);
+	q1.add(1);
 	Queue q2;
-	q2.add(3); q2.add(66); q2.add(128);  q2.add(5);
-	Queue q3 = q1+q2;
+	q2.add(3);
+	q2.add(66);
+	q2.add(128);
+	q2.add(5);
+	Queue q3 = q1 + q2;
 	q3.print();
 
-	return 0; 
-} 
+	return 0;
+}
