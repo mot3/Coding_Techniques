f8bef5b6190db29fe23c117652888124acc5f112
mot3 <hopdrmot3@gmail.com>
2021-04-05 01:16:32 +0430

    solve 6_Sum_square_difference.cpp

--- Euler projects/C++/6_Sum_square_difference.cpp
+++ Euler projects/C++/6_Sum_square_difference.cpp
@@ -6,6 +6,20 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    
+    int up = 100;
+    int sumOFsquare = 0, squareOFsum = 0;
+
+    for (size_t natural = 1; natural <= up; natural++)
+    {
+        sumOFsquare += pow(natural, 2);
+        squareOFsum += natural;
+    }
+
+    squareOFsum = pow(squareOFsum, 2);
+
+    int difference = squareOFsum - sumOFsquare;
+
+    cout << difference << endl;
+
     return 0;
 }
