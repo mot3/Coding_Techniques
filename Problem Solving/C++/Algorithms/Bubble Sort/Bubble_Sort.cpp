6e3dc669c5e89ce44db39164d24d203494d56701
mot3 <hopdrmot3@gmail.com>
2021-04-20 09:25:56 +0430

    Fix

--- Problem Solving/C++/Algorithms/Bubble Sort/Bubble_Sort.cpp
+++ Problem Solving/C++/Algorithms/Bubble Sort/Bubble_Sort.cpp
@@ -9,6 +9,49 @@
 
 // Output Format Print the required answer in a single line
 
-int arr[N];
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void bubble_sort(int *A, int n)
+{
+    int temp, counter = 0;
+    for (int k = 0; k < n - 1; k++)
+    {
+        // (n-k-1) is for ignoring comparisons of elements which have already been compared in earlier iterations
+
+        for (int i = 0; i < n - k - 1; i++)
+        {
+            if (A[i] > A[i + 1])
+            {
+                // here swapping of positions is being done.
+                temp = A[i];
+                A[i] = A[i + 1];
+                A[i + 1] = temp;
+                // swap(A[i], A[i+1]);
+                counter++;
+            }
+        }
+    }
+    cout << counter << endl;
+}
+
+void getArray(int *arr, size_t N)
+{
+    for (size_t i = 0; i < N; i++)
+        cin >> arr[i];
+}
+
+int main(int argc, char const *argv[])
+{
+    int N;
+    cin >> N;
+
+    int arr[N];
 
     getArray(arr, N);
+
+    bubble_sort(arr, N);
+
+    return 0;
+}
